e801f7e2acf493048da370b40b0024ca1793414b
MATHIEU FONTAINE <mathieufontaine@mathieus-macbook-pro.home>
2020-10-26 16:20:43 +0100

    added gsap animations

--- public/js/main.js
+++ public/js/main.js
@@ -21,5 +21,54 @@ hamburger.addEventListener("click", openMenu);
 close.addEventListener("click", closeMenu);
 overlay.addEventListener("click", closeMenu);
 
-// init Animation on Scroll
-AOS.init();
+// Animation on Scroll
+
+gsap.registerPlugin(ScrollTrigger);
+
+const topContentAnimation = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".content-top",
+      start: "center 80%",
+      end: "center 20%",
+      // toggleActions: "play none none reset",
+      defaults: {
+        duration: 2,
+        ease: "power1.inOut"
+      }
+    }
+  });
+
+  tl.from(".content-top", { x: "-150%", duration: 1 });
+  tl.from(".box", {
+    x: "10%",
+    opacity: 0,
+    scale: 0,
+    duration: 1.5,
+    stagger: 1
+  });
+};
+
+const articlesAnimation = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".articles",
+      start: "center 80%",
+      end: "center 20%",
+      defaults: {
+        ease: "power1.inOut"
+      }
+    }
+  });
+
+  tl.from(".articles h2", { x: "-150%", duration: 1 });
+  tl.from(".article", {
+    x: "150%",
+    opacity: 0,
+    duration: 1.5,
+    stagger: 1
+  });
+};
+
+topContentAnimation();
+articlesAnimation();
